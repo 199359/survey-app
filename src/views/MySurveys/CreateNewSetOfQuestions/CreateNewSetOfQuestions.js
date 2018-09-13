607d4c7aa1e2d89df37c0dc6c2b0b80327e263d4
robert019283 <piotrowskirobert7@gmail.com>
2018-09-13 19:28:03 +0200

    action buttons

--- src/views/MySurveys/CreateNewSetOfQuestions/CreateNewSetOfQuestions.js
+++ src/views/MySurveys/CreateNewSetOfQuestions/CreateNewSetOfQuestions.js
@@ -6,34 +6,53 @@ import FloatingActionButton from 'material-ui/FloatingActionButton';
 import ContentAdd from 'material-ui/svg-icons/content/add';
 import { TrashIcon } from '../../../icons/TrashIcon'
 import CustomTextField from '../../../components/SurveyElements/CustomTextField'
-import {toggleDialogWindow} from '../../../state/createNewSetOfQuestions'
+import { toggleDialogWindow } from '../../../state/createNewSetOfQuestions'
 import Dialog from 'material-ui/Dialog';
-import FlatButton from 'material-ui/FlatButton';
-import DatePicker from 'material-ui/DatePicker';
+
 
 const style = {
     margin: 10,
 }
 
-const icons = {
-    trash: "M 11 -0.03125 C 10.164063 -0.03125 9.34375 0.132813 8.75 0.71875 C 8.15625 1.304688 7.96875 2.136719 7.96875 3 L 4 3 C 3.449219 3 3 3.449219 3 4 L 2 4 L 2 6 L 24 6 L 24 4 L 23 4 C 23 3.449219 22.550781 3 22 3 L 18.03125 3 C 18.03125 2.136719 17.84375 1.304688 17.25 0.71875 C 16.65625 0.132813 15.835938 -0.03125 15 -0.03125 Z M 11 2.03125 L 15 2.03125 C 15.546875 2.03125 15.71875 2.160156 15.78125 2.21875 C 15.84375 2.277344 15.96875 2.441406 15.96875 3 L 10.03125 3 C 10.03125 2.441406 10.15625 2.277344 10.21875 2.21875 C 10.28125 2.160156 10.453125 2.03125 11 2.03125 Z M 4 7 L 4 23 C 4 24.652344 5.347656 26 7 26 L 19 26 C 20.652344 26 22 24.652344 22 23 L 22 7 Z M 8 10 L 10 10 L 10 22 L 8 22 Z M 12 10 L 14 10 L 14 22 L 12 22 Z M 16 10 L 18 10 L 18 22 L 16 22 Z "
-}
 
-const CreateNewSetOfQuestions = props => (
+
+class CreateNewSetOfQuestions extends React.Component {
+    
+    actions = [
+        <RaisedButton
+            label="CANCEL"
+            onClick={() => this.props._toggleDialogWindow()}
+        />,
+        <RaisedButton
+            label="ADD ELEMENT"
+            secondary={true}
+            style={style} />
+    ];
+
+    render() {
+        return(
     <div className='CreateNewSetOfQuestions-container'>
         <div>
             <form>
-                <CustomTextField/>
-                <a onClick={()=> alert('tash')} hoverColor='red100'>
-                <TrashIcon/>
+                <CustomTextField />
+                <a onClick={() => alert('tash')} hoverColor='red100'>
+                    <TrashIcon />
                 </a>
             </form>
         </div>
         <div>
             <FloatingActionButton mini={true} style={style} >
-                <ContentAdd  onClick={()=>props._toggleDialogWindow()}/>
+                <ContentAdd onClick={() => this.props._toggleDialogWindow()} />
             </FloatingActionButton>
-            
+            <Dialog
+                title="Choose new element"
+                actions={this.actions}
+                modal={false}
+                open={this.props._isDialogWindowOpen}
+                onRequestClose={this.handleClose}
+            >
+                Open a Date Picker dialog from within a dialog.
+            </Dialog>
         </div>
         <div>
             <Link
@@ -44,9 +63,10 @@ const CreateNewSetOfQuestions = props => (
             <RaisedButton className='create_new_set_of_questions create_new_set_of_questions-create_button' secondary={true} label="CREATE" />
         </div>
     </div>
-)
+)}}
 
 const mapStateToProps = state => ({
+    _isDialogWindowOpen: state.createNewSetOfQuestiones.isDialogWindowOpen
 })
 
 const mapDispatchToProps = dispatch => ({
